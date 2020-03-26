8b51e5cc1d7990822f17dafa66dc82f27e5f76ac
Kelvin <kelvindsmn@gmail.com>
2020-03-27 05:52:46 +0700

    deploy

---
+++ assets/js/12.21c6a15b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{173:function(t,n,e){"use strict";e.r(n);var s=e(27),l=Object(s.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("JournalList")],1)}),[],!1,null,null,null);n.default=l.exports}}]);
